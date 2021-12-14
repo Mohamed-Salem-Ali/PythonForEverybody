796fb5682e72dae06bfd12c8fb84c086e0283262
Mohamed-Salem-Ali <mohamedsalem0100411@sci.asu.edu.eg>
2021-12-14 06:17:52 +0200

    Update URL.py

--- WebAccess/Week2/URL.py
+++ WebAccess/Week2/URL.py
@@ -1,12 +1,12 @@
 import urllib.request 
 
 fhand = urllib.request.urlopen('http://data.pr4e.org/intro-short.txt')
-for line in fhand:
-    print(line.decode().strip())
-print('$'*20)
+
 counts = dict()
-for x in fhand:
-    words=x.decode().strip()
+for line in fhand:
+    words=line.decode().strip()
     for word in words:
         counts[word]= counts.get(word,0)+1
 print(counts)
+# print(words +'\n')
+# print(word)    
