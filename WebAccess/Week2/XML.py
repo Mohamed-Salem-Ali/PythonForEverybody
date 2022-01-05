32fa29bf34951f404d9c82a356c563ff4f125ce8
Mohamed-Salem-Ali <mohamedsalem0100411@sci.asu.edu.eg>
2022-01-05 17:02:56 +0200

    .

---
+++ WebAccess/Week2/XML.py
@@ -0,0 +1,9 @@
+# eXtensible Markup Language XML
+# [
+# (1)Start tag { <person> , <name> , <phone> }
+# (2) End tag { </person> , </name> , </phone> }
+# (3) Text Content { Chunk , salem , ali }
+# (4) Attribute { <phone type="int" hide="yes"> }
+# (5) Self Closing Tag { <email hide="yes" /> }
+# ]
+
